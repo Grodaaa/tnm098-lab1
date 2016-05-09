4d4385d88ab2a91eed6be9a4ea8e2c99d5da2482
Grodaaa <engstrom.kristinaa@gmail.com>
2016-05-09 16:05:17 +0200

    Fixade hur dte ser ut

--- lab3/Lab3.2/src/main.cpp
+++ lab3/Lab3.2/src/main.cpp
@@ -15,38 +15,19 @@
 using namespace std;
 
 void convert();
+void insertHash();
 
 int main()
 {
     convert();
-    
-    // Create 26 Items to store in the Hash Table.
-    Item * A = new Item {"Apple", NULL};
-    Item * B = new Item {"Banana", NULL};
-    Item * C = new Item {"Citron", NULL};
-    //Item * D = new Item {"Hejsan", NULL};
-    //Item * E = new Item {"Tomaterna", NULL};
-    //Item * F = new Item {"Apelsiner", NULL};
-
-    // Create a Hash Table of 13 Linked List elements.
-    HashTable table;
-    
-    // Add 3 Items to Hash Table.
-    table.insertItem(A, table.getNumberOfItems());
-    table.insertItem(B, table.getNumberOfItems());
-    table.insertItem(C, table.getNumberOfItems());
-    /*table.insertItem(D);
-    table.insertItem(E);
-    table.insertItem(F);*/
-    table.printTable();
-    //table.printHistogram();
-    
-    // Remove one item from Hash Table.
-    //table.removeItem("Apple");
-    
-    // Look up an item in the hash table
-    //Item * result = table.getItemByKey("Snakes");
-    //cout << result -> key << endl;
+    insertHash();
+    /*Det vi vill göra:
+    Läsa in filer och ta bort punkter + göra om till små bokstäver - CHECK!
+    Lägga in ord från filerna i hashtabellen
+        Om ordet inte finns ska det läggas in sist
+        Om ordet finns ska inget göras
+    Göra om meningarna i filerna till sifferkombinationer
+    Jämföra filerna*/
     return 0;
 }
 
@@ -100,4 +81,32 @@ void convert()
         is.close(); 
         outfile.close();         
     }
+}
+
+void insertHash()
+{
+    // Create 26 Items to store in the Hash Table.
+    Item * A = new Item {"Apple", NULL};
+    Item * B = new Item {"Banana", NULL};
+    Item * C = new Item {"Citron", NULL};
+
+    // Create a Hash Table of 13 Linked List elements.
+    HashTable table;
+    
+    // Add 3 Items to Hash Table.
+    table.insertItem(A, table.getNumberOfItems());
+    table.insertItem(B, table.getNumberOfItems());
+    table.insertItem(C, table.getNumberOfItems());
+    /*table.insertItem(D);
+    table.insertItem(E);
+    table.insertItem(F);*/
+    table.printTable();
+    //table.printHistogram();
+    
+    // Remove one item from Hash Table.
+    //table.removeItem("Apple");
+    
+    // Look up an item in the hash table
+    //Item * result = table.getItemByKey("Snakes");
+    //cout << result -> key << endl;
 }
